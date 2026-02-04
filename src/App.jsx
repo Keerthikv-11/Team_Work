c774d225244d70a67844914b2f71222854f5ec63
Keerthikv-11 <keerthikv018@gmail.com>
2026-02-05 02:04:54 +0530

    chnages

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
-import { useState } from 'react'
-import { Aboutme } from './pages/AboutMe'
+import { Home } from './pages/Home'
+import { AboutMe } from './pages/AboutMe'
 import { Projects } from './pages/Projects'
-import { Home} from './pages/Home'
-import { Techstack} from './pages/Techstack'
-import { Contact} from './pages/Contact'
-
-
+import { Techstack } from './pages/Techstack'
+import { Contact } from './pages/Contact'
 
 import './App.css'
-import { RouterProvider , createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-
+const routes = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/about", element: <AboutMe /> },
+  { path: "/techstack", element: <Techstack /> },
+  { path: "/projects", element: <Projects /> },
+  { path: "/contact", element: <Contact /> },
+  { path: "*", element: <h1>404 Page Not Found</h1> },
+])
 
 function App() {
-
-
-  return (
-    <>
-    <h1>Hello</h1>
-      <RouterProvider router={routes}>
-      
-      </RouterProvider>
-    </>
-  )
+  return <RouterProvider router={routes} />
 }
 
 export default App
